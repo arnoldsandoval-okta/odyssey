60ffc2b0a31b71e5912f9dd2424f4d9ebacfee78
Arnold Sandoval <arnold.sandoval@okta.com>
2020-09-11 23:41:36 -0700

    wip

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -3,6 +3,6 @@
 curl \
   -X POST \
   -H "Content-Type: application/json" \
-  --data '{"blocks": [{"type": "context","elements": [{"type": "mrkdwn","text": ":white_check_mark: *Preview Deployed*"}]},{"type": "section","text": {	"type": "mrkdwn",	"text": `> ${commitMessage}`}},{"type": "actions","elements": [	{"type": "button","text": {"type": "plain_text","text": "Pull Request #661","emoji": true},"url": "https://github.com/okta/odyssey/pull/'"$PULL_REQUEST_ID"'"	},	{"type": "button","style": "primary","text": {"type": "plain_text","text": "Visit Preview","emoji": true},"url": '"$PREVIEW_URL"'	}]},{"type": "context","elements": [	{"type": "mrkdwn","text": '"$AUTHOR_NAME"'@'"$SHA7"'	},	{"type": "mrkdwn","text": '"$BRANCH_NAME"'	}]},{"type": "divider"}]}' \
+  --data '{"blocks": [{"type": "context","elements": [{"type": "mrkdwn","text": ":white_check_mark: *Preview Deployed*"}]},{"type": "section","text": {	"type": "mrkdwn",	"text": `> '"$COMMIT_MESSAGE"'`}},{"type": "actions","elements": [	{"type": "button","text": {"type": "plain_text","text": "Pull Request #661","emoji": true},"url": "https://github.com/okta/odyssey/pull/'"$PULL_REQUEST_ID"'"	},	{"type": "button","style": "primary","text": {"type": "plain_text","text": "Visit Preview","emoji": true},"url": '"$PREVIEW_URL"'	}]},{"type": "context","elements": [	{"type": "mrkdwn","text": '"$AUTHOR_NAME"'@'"$SHA7"'	},	{"type": "mrkdwn","text": '"$BRANCH_NAME"'	}]},{"type": "divider"}]}' \
   $INCOMING_WEBHOOK_URL \
 
